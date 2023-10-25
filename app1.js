36a7b10bf964b4af9300359924d83c85a29ff65d
seonyo <jsy6535@naver.com>
2023-10-25 18:36:43 +0900

    ✨[Feat] nodemon install 및 headr로 text/plain 보내기

--- app1.js
+++ app1.js
@@ -4,9 +4,18 @@ const app = express()
 //포트 번호
 const port = 3000      
 
+// GET 요청
+// 주소 "/" 
+// req => 요청 메시지 객체, res => 응답 메시지 객체
 app.get('/', (req, res) => {
     res.send("Hello World!")
 })
+
+app.get('/hello', (req, res) => {
+    res.header("Content-Type", "text/plain")
+    res.send("<h1>Hello</h1>")
+})
+
 app.listen(port , ()=> {
     console.log(`Example app listening on port ${port}`);
 })
